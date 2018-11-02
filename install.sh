e2d04c36eb4224f6d9768d82c43e17482968b00c
Orkun Kulce <orkun@vngrs.com>
2018-11-03 02:27:25 +0300

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew cask install iterm2
+
+brew install fasd
+
+brew install ag
+
+brew install bat
