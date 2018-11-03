d9325096f217c238408edaad51e7a60f4097d7bc
Orkun Kulce <orkun@vngrs.com>
2018-11-03 15:14:41 +0300

    Ready

--- install.sh
+++ install.sh
@@ -1,25 +1,55 @@
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+sudo -v
+
+yes '' | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew tap caskroom/cask
 
+brew install git
+brew install zsh 
+brew install zsh-completions
+brew install fasd
+brew install fzf
+brew install the_silver_searcher
+brew install bat
+brew install httpie
+brew install jenv
+brew install jq
+brew install magic-wormhole
+brew install sbt
+brew install tig
+brew install youtube-dl
+brew install vagrant
+brew install vagrant-completion
+brew install stow
+brew install docker
+
+dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+curl https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/themes/half-life.zsh-theme > ~/.oh-my-zsh/custom/themes/half-life.zsh-theme
+
 brew cask install java
 
-brew install stow git zsh zsh-completions fasd fzf the_silver_searcher bat httpie jenv jq magic-wormhole sbt tig youtube-dl vagrant
+brew cask install iterm2
+brew cask install sublime-text
+brew cask install google-chrome
+brew cask install intellij-idea
+brew cask install calibre
+brew cask install karabiner
+brew cask install bitbar
+brew cask install postman
+brew cask install spotify
+brew cask install telegram
+brew cask install flycut
+brew cask install typora
+brew cask install tunnelblick
+brew cask install spectacle
 
-sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-curl https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/themes/half-life.zsh-theme > ~/.oh-my-zsh/themes/half-life.zsh-theme
-
-brew cask install iterm2 sublime-text google-chrome intellij-idea calibre karabiner
-brew cask install bitbar postman spotify telegram flycut typora tunnelblick spectacle
-
-cp iterm/* ~/Library/Preferences
-cp spectacle/Shortcuts ~/Library/Application Support/Spectacle
-cp bitbarplugins ~/Documents/
-cp karabiner ~/.config/
+cp -rf iterm/* ~/Library/Preferences
+cp -rf spectacle/Shortcuts ~/Library/Application Support/Spectacle
+cp -rf bitbarplugins ~/Documents/
+cp -rf karabiner ~/.config/
 
 set -x
 
 stow -t ~ git
-stow -t ~ fzf
 stow -t ~ zsh
--- zsh/.zsh.d/04-oh-my-zsh.sh
+++ zsh/.zsh.d/04-oh-my-zsh.sh
@@ -1,3 +1,5 @@
+ZSH="$HOME/.oh-my-zsh"
+
 plugins=(git httpie)
 
 if [[ "$OSTYPE" = darwin* ]]; then
@@ -5,7 +7,6 @@ if [[ "$OSTYPE" = darwin* ]]; then
 fi
 
 DISABLE_UPDATE_PROMPT=true
-
 
 ZSH_THEME="half-life"
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(root_indicator context dir vcs)
@@ -18,8 +19,7 @@ POWERLEVEL9K_TIME_FORMAT="%D{%H:%M - %d.%m.%y}"
 POWERLEVEL9K_STATUS_ERROR_BACKGROUND="red"
 POWERLEVEL9K_STATUS_ERROR_FOREGROUND="black"
 
-PROMPT=$'%{$purple%}%n%{$reset_color%} in %{$limegreen%}%~%{$reset_color%}$vcs_info_msg_0_%{$orange%} λ%{$reset_color%} '
-
-ZSH="$HOME/.oh-my-zsh"
 fpath=($ZSH/functions $ZSH/completions $fpath)
+
 source $ZSH/oh-my-zsh.sh
+PROMPT=$'%{$purple%}%n%{$reset_color%} in %{$limegreen%}%~%{$reset_color%}$vcs_info_msg_0_%{$orange%} λ%{$reset_color%} '
