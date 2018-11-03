925d114bd904534bb48461c2e6baf125bc9eb3fa
Orkun Kulce <orkun@vngrs.com>
2018-11-03 03:47:55 +0300

    Add profiles and brew installations

--- install.sh
+++ install.sh
@@ -1,9 +1,41 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew tap caskroom/cask
+
+brew cask install java
+
+brew install git zsh zsh-completions fasd fzf the_silver_searcher bat httpie jenv jq magic-wormhole sbt tig youtube-dl
+
+sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 brew cask install iterm2
 
-brew install fasd
+brew cask install spectacle ##import config
 
-brew install ag
+brew cask install sublime-text ##import config
 
-brew install bat
+brew cask install google-chrome
+
+brew cask install intellij-idea ##import config
+
+brew cask install bitbar ##add bitbar plugins to dotfiles
+
+brew cask install calibre ##import config
+
+brew cask install karabiner
+
+brew cask install postman
+
+brew cask install spotify
+
+brew cask install telegram 
+
+brew cask install flycut
+
+brew cask install typora
+
+brew cask install tunnelblick 
+
+set -x
+
---
+++ zsh/.zsh.d/04-oh-my-zsh.sh
@@ -0,0 +1,7 @@
+DISABLE_UPDATE_PROMPT=true
+
+ZSH_THEME="robbyrussell"
+
+ZSH="$HOME/.oh-my-zsh"
+
+source $ZSH/oh-my-zsh.sh
