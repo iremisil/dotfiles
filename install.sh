89c2b9fd4502005d31addf69caf8288675a63d0e
Orkun Kulce <orkun@vngrs.com>
2018-11-03 04:33:36 +0300

    Add git

--- install.sh
+++ install.sh
@@ -9,32 +9,12 @@ brew install git zsh zsh-completions fasd fzf the_silver_searcher bat httpie jen
 sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-brew cask install iterm2
-mv iterm/* $HOME/Library/Preferences
+brew cask install iterm2 sublime-text google-chrome intellij-idea calibre karabiner
+brew cask install bitbar postman spotify telegram flycut typora tunnelblick spectacle
 
-brew cask install spectacle ##import config
-cp spectacle/Shortcuts $HOME/Library/Application Support/Spectacle
-
-brew cask install sublime-text google-chrome intellij-idea calibre
-
-brew cask install bitbar 
-cp bitbarplugins $HOME/Documents/
-
-brew cask install calibre
-
-brew cask install karabiner
-
-brew cask install postman
-
-brew cask install spotify
-
-brew cask install telegram 
-
-brew cask install flycut
-
-brew cask install typora
-
-brew cask install tunnelblick 
+cp iterm/* ~/Library/Preferences
+cp spectacle/Shortcuts ~/Library/Application Support/Spectacle
+cp bitbarplugins ~/Documents/
+cp karabiner ~/.config/
 
 set -x
-
